d2e150d8e735167fcd45c0ad8520ae873442e917
pranavadurai <pranavadurai@gmail.com>
2019-02-24 01:49:11 +0530

    Restriction in Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     scope :api, defaults: { format: :json } do
-      resources :songs
+      resources :songs, only: [:index, :show]
     end
 end
